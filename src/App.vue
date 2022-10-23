d3b93a9e6c24eafe3868b66c8e81ad45fc0aead2
Enflares <info@enflares.com>
2022-10-23 18:42:49 +1100

    npm install --save bootstrap npm install --save @popperjs/core
    https://stackoverflow.com/questions/65547199/using-bootstrap-5-with-vue-3

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,17 @@ import HelloWorld from "./components/HelloWorld.vue";
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <button
+          class="btn btn-primary"
+          data-bs-target="#collapseTarget"
+          data-bs-toggle="collapse">
+        Bootstrap collapse
+      </button>
+      <div class="collapse py-2" id="collapseTarget">
+        This is the toggle-able content!
+      </div>
+
     </div>
   </header>
 
